f0c3de170d3317eb81d53ba45c5bca030bc06d22
henriquetrader281-pixel <henriquetrader281@gmail.com>
2026-04-06 18:38:59 -0300

    Create mineracao.py

---
+++ mineracao.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def minerar_produtos(segmento):
+    """Função para minerar produtos virais"""
+    return [f"Produto Viral 1 {segmento}", f"Produto Viral 2 {segmento}"]
+
+def extrair_dados_shopee(link):
+    """Extrai dados da Shopee"""
+    return {"nome": "Produto Exemplo", "preco": "R$ 49,90"}
